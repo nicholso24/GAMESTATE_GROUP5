e3c934da88efdace05e8be4474ab99e2ae3f9ad2
chiara <profenna23@up.edu>
2021-03-11 19:43:53 -0800

    fixed comments in onClick method

--- app/src/main/java/com/example/gamestate_group5/MainActivity.java
+++ app/src/main/java/com/example/gamestate_group5/MainActivity.java
@@ -21,48 +21,41 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                // 1) Any text currently displayed in in the multi-line EditText (probably from a
-                // previous test run) should be cleared.
+                // clears the multi-line EditText
                 EditText multilineText = (EditText) findViewById(R.id.editTextTextMultiLine);
                 multilineText.setText("");
 
-                // 2) A new instance of the game state class is created using the default constructor and
-                // assigned to a variable named firstInstance.
+                // new instance of game state class
                 GameState firstInstance = new GameState();
 
-                // 3) Use your other constructor to create a deep copy of firstInstance from the perspective
-                // of player one. Assign this copy to a variable named secondInstance.
+                // deep copy of firstInstance
                 GameState secondInstance = new GameState(firstInstance);
 
-                // 4) Using firstInstance, call each method in the game state class at least once. In
-                // each case it should be making a legal move in the game.
-                // For each method call, a brief description of the action taken should be
-                // printed to the multi-line EditText.
-                // New messages should be appended to previous ones, not overwrite them.
-                firstInstance.playCard();
+                // calls game state methods
+                firstInstance.playTest();
+
+                // NEED DESCRIPTIONS OF ACTIONS PRINTED TO MULTI-LINE EDITTEXT
+
+                /*firstInstance.playCard();
                 multilineText.append("player plays card on discard pile\n");
                 firstInstance.drawCard();
                 multilineText.append("player draws card from drawing pile\n");
                 firstInstance.sayUno();
                 multilineText.append("player presses uno button, indicates 1 card in hand left\n");
                 firstInstance.useWild();
-                multilineText.append("player has used a wild card to change the color\n");
+                multilineText.append("player has used a wild card to change the color\n");*/
 
-                // 5) Create a new instance of the game state class using the default constructor.
-                // Assign this value to a variable named thirdInstance.
+                // creates another new instance of game state class
                 GameState thirdInstance = new GameState();
 
-                // 6) Use your deep copy constructor to make a deep copyof thirdInstance from
-                // the perspective of player one. Assign this copy to a variable named fourthInstance.
-                // need deep opy to be completed first
+                // deep copy of thirdInstance
                 GameState fourthInstance = new GameState(thirdInstance);
 
-                // 7) Call the toString() method on secondInstance and fourthInstance.
-                // The two strings should be identical. Your code should verify this.
-                // Also, print both strings to the multi-line EditText for visual inspection.
-                // Again, append these rather than overwrite previous messages.
+                // calls toString() method on secondInstance and fourthInstance; prints to screen
                 secondInstance.toString();
+                multilineText.append("second instance: "+secondInstance.toString());
                 fourthInstance.toString();
+                multilineText.append("fourth instance: "+fourthInstance.toString());
 
             }
         });
